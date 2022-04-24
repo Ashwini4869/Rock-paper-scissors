3e860cd1fff84bdfa72915db1e48e67967b67f40
Ashwini <076bct012.ashwini@pcampus.edu.np>
2022-04-24 22:25:57 +0545

    skeleton complete..js to be started

--- scripts.js
+++ scripts.js
@@ -1,69 +1,64 @@
-var c=0,p=0;
+// var c = 0,
+//   p = 0;
 
-let computerPlay = function () { 
-  const randomNumber = Math.floor(Math.random() * 3);
+// let computerPlay = function () {
+//   const randomNumber = Math.floor(Math.random() * 3);
 
-   if (randomNumber == 0) {
-    return "rock";
-  }
-  if (randomNumber == 1) {
-    return "paper";
-  }
-  if (randomNumber == 2) {
-    return "scissors";
-} 
-};
-let user = function () {
-  let sign = prompt("Enter your selection: Rock,paper or scissors?");
-  return sign.toLowerCase();
-};
-function playRound(playerSelection, computerSelection) {
-  if ((playerSelection == "rock") && (computerSelection == "paper")) {
-    c++;
-    return "You lose!Paper beats rock";
-    
-  } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-    p++;
-    return "You win!Paper beats rock";
-  } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-    p++;
-    return "You win!scissors beats paper";
-  } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-    c++;
-    return "You lose!scissors beats paper";
-  } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-    p++;
-    return "You win!rock beats scissors";
-  } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-    c++;
-    return "You lose!rock beats scissors";
-  } else if ((playerSelection === computerSelection)) 
-  {
-    return "Its a tie!";
-  }
-  else{
-    return "Enter a valid selection";
-  }
-}
+//   if (randomNumber == 0) {
+//     return "rock";
+//   }
+//   if (randomNumber == 1) {
+//     return "paper";
+//   }
+//   if (randomNumber == 2) {
+//     return "scissors";
+//   }
+// };
+// let user = function () {
+//   let sign = prompt("Enter your selection: Rock,paper or scissors?");
+//   return sign.toLowerCase();
+// };
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection == "rock" && computerSelection == "paper") {
+//     c++;
+//     return "You lose!Paper beats rock";
+//   } else if (playerSelection == "paper" && computerSelection == "rock") {
+//     p++;
+//     return "You win!Paper beats rock";
+//   } else if (playerSelection == "scissors" && computerSelection == "paper") {
+//     p++;
+//     return "You win!scissors beats paper";
+//   } else if (playerSelection == "paper" && computerSelection == "scissors") {
+//     c++;
+//     return "You lose!scissors beats paper";
+//   } else if (playerSelection == "rock" && computerSelection == "scissors") {
+//     p++;
+//     return "You win!rock beats scissors";
+//   } else if (playerSelection == "scissors" && computerSelection == "rock") {
+//     c++;
+//     return "You lose!rock beats scissors";
+//   } else if (playerSelection === computerSelection) {
+//     return "Its a tie!";
+//   } else {
+//     return "Enter a valid selection";
+//   }
+// }
 
-function game(){
-  for(let k=0;k<5;k++){
-const playerSelection = user();
-const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-  }
-  console.log("Final Scores:");
-  console.log("computer score=" + c);
-  console.log("Player's score=" + p);
+// function game() {
+//   const playerSelection = user();
+//   const computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection));
 
-if(c>p){
-  console.log("you lose!");
-}
-else if(c<p){
-  console.log("You win!");
-}
-else{
-  console.log("Game ends in tie!");
-}
-}
-game();
+//   console.log("Final Scores:");
+//   console.log("computer score=" + c);
+//   console.log("Player's score=" + p);
+
+//   if (c > p) {
+//     console.log("you lose!");
+//   } else if (c < p) {
+//     console.log("You win!");
+//   } else {
+//     console.log("Game ends in tie!");
+//   }
+// }
+// game();
