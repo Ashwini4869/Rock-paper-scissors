a388cf46efd4873b276a01a47059aa9dc3d79629
Ashwini <076bct012.ashwini@pcampus.edu.np>
2022-04-27 00:05:14 +0545

    implemented reset feature

--- scripts.js
+++ scripts.js
@@ -1,67 +1,3 @@
-// var c = 0,
-//   p = 0;
-
-// let computerPlay = function () {
-//   const randomNumber = Math.floor(Math.random() * 3);
-
-//   if (randomNumber == 0) {
-//     return "rock";
-//   }
-//   if (randomNumber == 1) {
-//     return "paper";
-//   }
-//   if (randomNumber == 2) {
-//     return "scissors";
-//   }
-// };
-// let user = function () {
-//   let sign = prompt("Enter your selection: Rock,paper or scissors?");
-//   return sign.toLowerCase();
-// };
-// function playRound(playerSelection, computerSelection) {
-//   if (playerSelection == "rock" && computerSelection == "paper") {
-//     c++;
-//     return "You lose!Paper beats rock";
-//   } else if (playerSelection == "paper" && computerSelection == "rock") {
-//     p++;
-//     return "You win!Paper beats rock";
-//   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-//     p++;
-//     return "You win!scissors beats paper";
-//   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-//     c++;
-//     return "You lose!scissors beats paper";
-//   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-//     p++;
-//     return "You win!rock beats scissors";
-//   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-//     c++;
-//     return "You lose!rock beats scissors";
-//   } else if (playerSelection === computerSelection) {
-//     return "Its a tie!";
-//   } else {
-//     return "Enter a valid selection";
-//   }
-// }
-
-// function game() {
-//   const playerSelection = user();
-//   const computerSelection = computerPlay();
-//   console.log(playRound(playerSelection, computerSelection));
-
-//   console.log("Final Scores:");
-//   console.log("computer score=" + c);
-//   console.log("Player's score=" + p);
-
-//   if (c > p) {
-//     console.log("you lose!");
-//   } else if (c < p) {
-//     console.log("You win!");
-//   } else {
-//     console.log("Game ends in tie!");
-//   }
-// }
-// game();
 function computerSelection() {
   return Math.floor(Math.random() * 3);
 }
@@ -69,6 +5,7 @@ function computerSelection() {
 const button1 = document.querySelector("button#b1");
 const button2 = document.querySelector("button#b2");
 const button3 = document.querySelector("button#b3");
+const reset = document.querySelector("button#reset");
 //score display
 let pdisplay = document.querySelector("span#pScore");
 let cdisplay = document.querySelector("span#cScore");
@@ -147,4 +84,13 @@ button3.addEventListener("click", function () {
       gameOver = true;
     }
   }
+});
+
+reset.addEventListener("click", function () {
+  gameOver = false;
+  pScore = 0;
+  cScore = 0;
+  pdisplay.textContent = pScore;
+  cdisplay.textContent = cScore;
+  document.querySelector("p#winner").textContent = "";
 });
